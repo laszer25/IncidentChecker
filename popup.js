74d2950d8a3615bf12bc4be6b84058efc07330c7
Sisir Sagar <sisirsagar@gmail.com>
2016-06-03 23:18:51 +0530

    Changing names and trying console log

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+  var startTrackingButton = document.getElementById('startTracking');
+  startTrackingButton.addEventListener('click', function() {
 
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
-
+      console.log(d);
       var f = d.createElement('form');
       f.action = 'http://gtmetrix.com/analyze.html?bm';
       f.method = 'post';
