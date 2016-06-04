8f9445de7e8fbc465e507e5c22e2e2cf1ead1fc0
Sisir Sagar <sisirsagar@gmail.com>
2016-06-04 14:56:50 +0530

    Styling and disabling the button for starting alert

--- popup.js
+++ popup.js
@@ -1,17 +1,25 @@
-
+var bgPage = chrome.extension.getBackgroundPage();
 document.addEventListener('DOMContentLoaded', function() {
   var startTrackingButton = document.getElementById('startTracking');
   startTrackingButton.addEventListener('click', function() {
     console.log("clicked");
     startTrackingButton.innerText = "Tracking...meow..meow"
+    startTrackingButton.disabled = true
     chrome.tabs.getSelected(null, function(tab) {
       
       chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, function(response) {
         console.log("response received")
         console.log(response)
-        var bgPage = chrome.extension.getBackgroundPage();
         bgPage.doAwesomeStuffWithTheResponse(response, tab);
       });      
     });
   }, false);
 }, false);
+window.onload = function() {
+  var isTracking = bgPage.isTracking();
+  var startTrackingButton = document.getElementById('startTracking');
+  if (isTracking) {
+    startTrackingButton.innerText = "Tracking...meow..meow"
+    startTrackingButton.disabled = true
+  }
+}
